492720f1105fa47096972c81224525ac78e9c6c9
Cao Khai <goku1331999@gmail.com>
2025-03-07 15:14:08 +0700

    latest update

--- backupOnline.sh
+++ backupOnline.sh
@@ -2,4 +2,5 @@ git config --global --unset http.proxy
 git fetch origin
     git add *.h *.cpp *.sh *.txt ./include/*.h
     git commit -m "latest update"
+    git merge main
     git push -u origin main
