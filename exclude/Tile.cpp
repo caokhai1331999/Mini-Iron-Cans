91b5c2222735d971c0cdf33441c17021482fa175
Cao Khai <goku1331999@gmail.com>
2025-03-10 10:54:30 +0700

    Add gameplay demo gif( for my portfolio)

---
+++ exclude/Tile.cpp
@@ -0,0 +1,38 @@
+#include "Tile.h"
+
+Tile::Tile( int x, int y, int tileType )
+{
+    //Get the offsets
+    mBox.x = x;
+    mBox.y = y;
+
+    //Set the collision box
+    mBox.w = SMALL_TILE_WIDTH;
+    mBox.h = SMALL_TILE_HEIGHT;
+
+    //Get the tile type
+    mType = tileType;
+}
+
+void Tile::render( SDL_Rect& camera, SDL_Renderer* gRenderer, LTexture* gTileTexture, SDL_Rect* gTileClips, bool checkCollision)
+{
+    // May be I pass the wrong type of gTileCLips
+    //If the tile is on screen
+    // if(checkCollision )
+    // {
+        //Show the tile
+        gTileTexture->render(gRenderer, mBox.x - camera.x, mBox.y - camera.y, &gTileClips[ mType ] );
+        // if (((mBox.x >= camera.x) && (mBox.x <= camera.x + camera.w)) &&((mBox.y >= camera.y) && (mBox.y <= camera.y + camera.w)) ){            
+        // }
+   // }
+}
+
+int Tile::getType()
+{
+    return mType;
+}
+
+SDL_Rect* Tile::getBox()
+{
+    return &mBox;
+}
---
+++ exclude/TileX.cpp
@@ -0,0 +1,21 @@
+/* ========================================================================
+   $File: $
+   $Date: $
+   $Revision: $
+   $Creator: Cao Khai(Casey Muratori's disciple) $
+   $Notice: (C) Copyright 2024 by Cao Khai, Inc. All Rights Reserved. $
+   ======================================================================== */
+#include "TileX.h"
+
+void renderTile( SDL_Rect& camera, SDL_Renderer* gRenderer, Tile tile,LTexture* gTileTexture, SDL_Rect* gTileClips, bool checkCollision)
+{
+    // May be I pass the wrong type of gTileCLips
+    //If the tile is on screen
+    // if(checkCollision )
+    // {
+        //Show the tile
+        gTileTexture->render(gRenderer, tile.mBox.x - camera.x, tile.mBox.y - camera.y, &gTileClips[ tile.mType ] );
+        // if (((mBox.x >= camera.x) && (mBox.x <= camera.x + camera.w)) &&((mBox.y >= camera.y) && (mBox.y <= camera.y + camera.w)) ){            
+        // }
+   // }
+}
