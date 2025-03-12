fe3f48fddc4d063a6046c98b6be57090eccad75a
Cao Khai <goku1331999@gmail.com>
2025-03-12 11:53:50 +0700

    remove unrelated files

--- src/PullNew.sh
+++
@@ -1,3 +0,0 @@
-git config --global --unset http.proxy
-git fetch origin
-git pull origin main
--- src/backupOnline.sh
+++
@@ -1,5 +0,0 @@
-git config --global --unset http.proxy
-git fetch origin
-    git add *.h *.cpp *.sh *.txt ./include/*.h
-    git commit -m "latest update"
-    git push -u origin main
