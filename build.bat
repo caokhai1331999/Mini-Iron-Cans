fb9ddf101382b7d637f18640e5ffba6519a29451
Cao Khai <goku1331999@gmail.com>
2025-03-14 15:05:16 +0700

    latest update

--- build.bat
+++ build.bat
@@ -1,14 +1,30 @@
 @echo off
 setlocal
 
+set hr=%time:~0,2%
+set hr=%hr: =0%
+set min=%time:~3,2%
+set min=%min: =0%
+set sec=%time:~6,2%
+set sec=%sec: =0%
+
+set dd=%date:~-4,4%
+set dd=%dd: =0%
+set mm=%date:~-10,2%
+set mm=%mm: =0%
+set yr=%date:~-7,2%
+set yr=%yr: =0%
+
 REM Define paths to SDL include, library, and DLL directories
 set DEFAULT_INCLUDE_DIR="..\include"
-set SDL_INCLUDE_DIR="C:\Users\Cao Khai\Downloads\External_Libraries\SDL2\SDL2-2.30.2\include"
-set SDL_LIB_DIR="C:\Users\Cao Khai\Downloads\External_Libraries\SDL2\SDL2-2.30.2\lib\x64"
-set SDL_DLL_DIR="C:\Users\Cao Khai\Downloads\External_Libraries\SDL2\SDL2-2.30.2\bin\x64"
+set COMMON_COMPILER_FLAG=/FC /Zi /EHsc /FD
+set INHERENT_LIB=SDL2.lib SDL2main.lib SDL2_image.lib SDL2_mixer.lib SDL2_ttf.lib shlwapi.lib Shell32.lib
+set SDL_INCLUDE_DIR="C:\Users\klove\Downloads\External_Libraries\SDL2\SDL2-2.30.2\include"
+set SDL_LIB_DIR="C:\Users\klove\Downloads\External_Libraries\SDL2\SDL2-2.30.2\lib\x64"
+set SDL_DLL_DIR="C:\Users\klove\Downloads\External_Libraries\SDL2\SDL2-2.30.2\bin\x64"
 
 REM Define source files and output executable
-set SRC_FILES=mainX.cpp
+set SRC_FILES=..\src\mainK.cpp
 set OUT_EXE=sdl_project.exe
 
 if not exist build (
@@ -23,7 +39,7 @@ rem del *.obj *.pdb
 REM List all .cpp files in the current directory
 setlocal enabledelayedexpansion
 
-REM Compile the project
+rem Compile the project
 for %%f in (..\src\*.cpp) do (
    set FILES=!FILES! %%f
  )
@@ -32,8 +48,8 @@ REM Add DLL directory to PATH for runtime
 set PATH=%DLL_DIR%;%PATH%
 
 REM /subsystem:consoles
-REM The reason why the compile the subsystem:consoles may solved the undebug problem. /fsanitize=address /FORCE:MULTIPLE
-cl %FILES% /FC /Zi /EHsc  /RTC1 /I %DEFAULT_INCLUDE_DIR% /I %SDL_INCLUDE_DIR% /link /LIBPATH:%SDL_LIB_DIR% /subsystem:console  SDL2.lib SDL2main.lib SDL2_image.lib SDL2_mixer.lib SDL2_ttf.lib shlwapi.lib Shell32.lib /FORCE:MULTIPLE /DEBUG /OUT:%OUT_EXE%
+REM The reason why the compile the subsystem:consoles may solved the undebug problem. /fsanitize=address /FORCE:MULTIPLE /PDB:TANK_%hr%%min%%sec%_%dd%%mm%%yr%.pdb /LD
+cl %FILES% %COMMON_COMPILER_FLAG% /I %DEFAULT_INCLUDE_DIR% /I %SDL_INCLUDE_DIR%  /link /LIBPATH:%SDL_LIB_DIR% /subsystem:console %INHERENT_LIB% /DEBUG /OUT:%OUT_EXE%
 
 if %ERRORLEVEL% EQU 0 (
    @echo Announce: compilation succeeded "(^ w ^)" at %time%
