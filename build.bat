3cc3942aa3b3d41d800983c90abbd444f0384f93
Cao Khai <goku1331999@gmail.com>
2025-03-17 22:22:39 +0700

    change build.bat but still not work

--- build.bat
+++ build.bat
@@ -17,13 +17,13 @@ set yr=%yr: =0%
 
 REM Define paths to SDL include, library, and DLL directories
 set DEFAULT_INCLUDE_DIR="..\include"
-set COMMON_COMPILER_FLAG=/FC /Zi /MD /EHsc
+set COMMON_COMPILER_FLAG=/FC /Zi /MD /LD /EHsc
 rem /EXPORT:Game /EXPORT:PlatformP
 set EXPORT_=/EXPORT:ProcessInput /EXPORT:Update /EXPORT:Render /EXPORT:Close
 set INHERENT_LIB=SDL2.lib SDL2main.lib SDL2_image.lib SDL2_mixer.lib SDL2_ttf.lib shlwapi.lib Shell32.lib
-set SDL_INCLUDE_DIR="C:\Users\klove\Downloads\External_Libraries\SDL2\SDL2-2.30.2\include"
-set SDL_LIB_DIR="C:\Users\klove\Downloads\External_Libraries\SDL2\SDL2-2.30.2\lib\x64"
-set SDL_DLL_DIR="C:\Users\klove\Downloads\External_Libraries\SDL2\SDL2-2.30.2\bin\x64"
+set SDL_INCLUDE_DIR="C:\Users\Cao Khai\Downloads\External_Libraries\SDL2\SDL2-2.30.2\include"
+set SDL_LIB_DIR="C:\Users\Cao Khai\Downloads\External_Libraries\SDL2\SDL2-2.30.2\lib\x64"
+set SDL_DLL_DIR="C:\Users\Cao Khai\Downloads\External_Libraries\SDL2\SDL2-2.30.2\bin\x64"
 
 REM Define source files and output executable
 set SRC_FILES=..\src\mainK.cpp
@@ -36,7 +36,7 @@ if not exist build (
 echo folder before pushd : %CD%
 pushd build
 echo folder after pushd : %CD%
-del *.obj *.pdb *.lib *.dll
+del *.obj *.pdb *.lib
 
 REM List all .cpp files in the current directory
 setlocal enabledelayedexpansion
@@ -51,8 +51,11 @@ set PATH=%DLL_DIR%;%PATH%
 
 REM /subsystem:consoles
 REM The reason why the compile the subsystem:consoles may solved the undebug problem. /fsanitize=address /FORCE:MULTIPLE /PDB:TANK_%hr%%min%%sec%_%dd%%mm%%yr%.pdb /LD /D _WIN64 ..\src\Gameh.cpp
-del *.pdb
-cl ..\src\*.cpp  %COMMON_COMPILER_FLAG% /LD /I %DEFAULT_INCLUDE_DIR% /I %SDL_INCLUDE_DIR% /link %EXPORT_% /PDB:main_%hr%%min%%sec%_%dd%%mm%%yr%.pdb /LIBPATH:%SDL_LIB_DIR% /subsystem:console %INHERENT_LIB% /DEBUG /OUT:%OUT_EXE%
+rem del *.pdb
+cl /LD ..\src\Gameh.cpp ..\src\GamePhysics.cpp ..\src\PlatformP.cpp ..\src\TileX.cpp ..\src\xTank.cpp ..\src\XTexture.cpp %COMMON_COMPILER_FLAG% /I %DEFAULT_INCLUDE_DIR% /I %SDL_INCLUDE_DIR% /link %EXPORT_% /PDB:main_%hr%%min%%sec%_%dd%%mm%%yr%.pdb /LIBPATH:%SDL_LIB_DIR% SDL2.lib SDL2_image.lib SDL2_mixer.lib SDL2_ttf.lib shlwapi.lib Shell32.lib /DEBUG /OUT:Gameh.dll
+
+
+cl ..\src\*.cpp %COMMON_COMPILER_FLAG% /I %DEFAULT_INCLUDE_DIR% /I %SDL_INCLUDE_DIR% /link  Gameh.dll %EXPORT_% /LIBPATH:%SDL_LIB_DIR% %INHERENT_LIB% /DEBUG /subsystem:console /OUT:%OUT_EXE%
 
 if %ERRORLEVEL% EQU 0 (
    @echo Announce: compilation succeeded "(^ w ^)" at %time%
